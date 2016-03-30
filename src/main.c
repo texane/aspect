34b7b9d6ab4fdb5946c6ca727488dba271d07579
texane <texane@gmail.com>
2016-03-30 23:06:30 +0200

    pcm_handle_t

--- src/main.c
+++ src/main.c
@@ -93,88 +93,112 @@ static int get_cmdline(cmdline_t* cmd, int ac, char** av)
 
 /* audio device */
 
-static snd_pcm_t* open_pcm
-(const cmdline_t* cmd, unsigned int is_in)
+typedef struct
 {
-  const char* name;
-  snd_pcm_hw_params_t* params;
-  snd_pcm_stream_t s;
   snd_pcm_t* pcm;
+  snd_pcm_hw_params_t* params;
+  snd_pcm_channel_area_t* areas;
+
+  size_t nchan;
+  size_t wchan;
+  size_t scale;
+
+  uint8_t* buf;
+  size_t rpos;
+  size_t wpos;
+  size_t nsampl;
+
+} pcm_handle_t;
+
+
+typedef struct
+{
+#define PCM_FLAG_IN (1 << 0)
+#define PCM_FLAG_OUT (1 << 1)
+  uint32_t flags;
+  const char* name;
+  size_t nchan;
+  unsigned int fsampl;
+} pcm_desc_t;
+
+
+static void pcm_init_desc(pcm_desc_t* desc)
+{
+  desc->flags = 0;
+  desc->name = "default";
+  desc->nchan = 2;
+  desc->fsampl = 44100;
+}
+
+
+static int pcm_open(pcm_handle_t* pcm, const pcm_desc_t* desc)
+{
+  const snd_pcm_format_t fmt = SND_PCM_FORMAT_S16_LE;
+  snd_pcm_stream_t stm;
   int err;
 
-  if (is_in)
-  {
-    s = SND_PCM_STREAM_CAPTURE;
-    if ((cmd->flags & CMDLINE_FLAG(IPCM)) == 0) name = "default";
-    else name = cmd->ipcm;
-  }
-  else
-  {
-    s = SND_PCM_STREAM_PLAYBACK;
-    if ((cmd->flags & CMDLINE_FLAG(OPCM)) == 0) name = "default";
-    else name = cmd->opcm;
-  }
+  if (desc->flags & PCM_FLAG_IN) stm = SND_PCM_STREAM_CAPTURE;
+  else stm = SND_PCM_STREAM_PLAYBACK;
 
-  err = snd_pcm_open(&pcm, name, s, 0);
+  err = snd_pcm_open
+    (&pcm->pcm, desc->name, stm, SND_PCM_NONBLOCK);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_0);
 
-  err = snd_pcm_hw_params_malloc(&params);
+  err = snd_pcm_hw_params_malloc(&pcm->params);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_1);
 
-  err = snd_pcm_hw_params_any(pcm, params);
+  err = snd_pcm_hw_params_any(pcm->pcm, pcm->params);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
 
   err = snd_pcm_hw_params_set_access
-    (pcm, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+    (pcm->pcm, pcm->params, SND_PCM_ACCESS_RW_INTERLEAVED);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
 
-  err = snd_pcm_hw_params_set_format
-    (pcm, params, SND_PCM_FORMAT_S16_LE);
+  err = snd_pcm_hw_params_set_format(pcm->pcm, pcm->params, fmt);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
 
-  err = snd_pcm_hw_params_set_rate(pcm, params, 44100, 0);
+  err = snd_pcm_hw_params_set_rate
+    (pcm->pcm, pcm->params, desc->fsampl, 0);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
 
-  err = snd_pcm_hw_params_set_channels(pcm, params, 2);
+  pcm->nchan = desc->nchan;
+  pcm->wchan = (size_t)snd_pcm_format_physical_width(fmt) / 8;
+  pcm->scale = pcm->nchan * pcm->wchan;
+
+  err = snd_pcm_hw_params_set_channels
+    (pcm->pcm, pcm->params, desc->nchan);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
   
-  err = snd_pcm_hw_params(pcm, params);
+  err = snd_pcm_hw_params(pcm->pcm, pcm->params);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
   
-  err = snd_pcm_prepare(pcm);
+  err = snd_pcm_prepare(pcm->pcm);
   if (err) PERROR_GOTO(snd_strerror(err), on_error_2);
 
-  snd_pcm_hw_params_free(params);
+  pcm->rpos = 0;
+  pcm->wpos = 0;
+  pcm->nsampl = (size_t)desc->fsampl * 1;
+  pcm->buf = malloc(pcm->nsampl * pcm->scale);
+  if (pcm->buf == NULL) goto on_error_2;
 
-  return pcm;
+  return 0;
 
  on_error_2:
-  snd_pcm_hw_params_free(params);
+  snd_pcm_hw_params_free(pcm->params);
  on_error_1:
-  snd_pcm_close(pcm);
+  snd_pcm_close(pcm->pcm);
  on_error_0:
-  return NULL;
+  return -1;
   
 }
 
-static snd_pcm_t* open_in_pcm(const cmdline_t* cmd)
+static void pcm_close(pcm_handle_t* pcm)
 {
-  static const unsigned int is_in = 1;
-  return open_pcm(cmd, is_in);
+  free(pcm->buf);
+  snd_pcm_hw_params_free(pcm->params);
+  snd_pcm_close(pcm->pcm);
 }
 
-static snd_pcm_t* open_out_pcm(const cmdline_t* cmd)
-{
-  static const unsigned int is_in = 0;
-  return open_pcm(cmd, is_in);
-}
-
-
-/* main loop */
-/* http://www.alsa-project.org/alsa-doc/alsa-lib
-/* /_2test_2pcm_8c-example.html */
-
-#if 0
 
 static int recover_xrun(snd_pcm_t* pcm, int err)
 {
@@ -208,6 +232,8 @@ static int recover_xrun(snd_pcm_t* pcm, int err)
   return -1;
 }
 
+#if 0
+
 static int process_pcm(snd_pcm_t* in_pcm, snd_pcm_t* out_pcm)
 {
   while (1)
@@ -218,6 +244,35 @@ static int process_pcm(snd_pcm_t* in_pcm, snd_pcm_t* out_pcm)
   return 0;
 }
 
+
+static snd_pcm_channel_area_t* alloc_buffer(size_t nchan, size_t nsampl)
+{
+  snd_pcm_channel_area_t* areas;
+  size_t i;
+
+  areas = malloc(nchan * sizeof(snd_pcm_channel_area_t));
+  if (areas == NULL) return -1;
+
+  for (i = 0; i != nchan; ++i)
+  {
+    areas[i].addr = 
+  }
+
+  
+
+  unsigned int nchan;
+
+  snd_pcm_hw_params_get_channels();
+
+  const size_t nchan = ;
+
+  for (chn = 0; chn < channels; chn++) {
+    areas[chn].addr = samples;
+    areas[chn].first = chn * snd_pcm_format_physical_width(format);
+    areas[chn].step = channels * snd_pcm_format_physical_width(format);
+  }
+}
+
 #endif
 
 
@@ -225,36 +280,81 @@ static int process_pcm(snd_pcm_t* in_pcm, snd_pcm_t* out_pcm)
 
 int main(int ac, char** av)
 {
-  snd_pcm_t* in_pcm;
-  snd_pcm_t* out_pcm;
+  pcm_desc_t desc;
+  pcm_handle_t ipcm;
+  pcm_handle_t opcm;
   int err;
-#define IN_FRAME_COUNT 2048
-#define IN_CHAN_COUNT 2
-  int16_t in_buf[IN_FRAME_COUNT * IN_CHAN_COUNT];
   cmdline_t cmd;
   size_t i;
 
+  err = -1;
+
   if (get_cmdline(&cmd, ac - 1, av + 1)) goto on_error_0;
 
-  in_pcm = open_in_pcm(&cmd);
-  if (in_pcm == NULL) goto on_error_0;
+  pcm_init_desc(&desc);
+  desc.flags |= PCM_FLAG_IN;
+  if (cmd.flags & CMDLINE_FLAG(IPCM)) desc.name = cmd.ipcm;
+  if (pcm_open(&ipcm, &desc)) goto on_error_0;
 
-  out_pcm = open_out_pcm(&cmd);
-  if (out_pcm == NULL) goto on_error_1;
-  
+  pcm_init_desc(&desc);
+  desc.flags |= PCM_FLAG_OUT;
+  if (cmd.flags & CMDLINE_FLAG(OPCM)) desc.name = cmd.opcm;
+  if (pcm_open(&opcm, &desc)) goto on_error_1;
+
   signal(SIGINT, on_sigint);
 
-  for (i = 0; is_sigint == 0; i += IN_FRAME_COUNT)
+  for (i = 0; is_sigint == 0; i += 1)
   {
-    err = snd_pcm_readi(in_pcm, in_buf, IN_FRAME_COUNT);
-    if (err != IN_FRAME_COUNT) PERROR_GOTO("", on_error_2);
-    snd_pcm_writei(out_pcm, in_buf, IN_FRAME_COUNT);
+    size_t nsampl;
+    size_t navail;
+    size_t off;
+
+    /* read ipcm */
+
+    if (ipcm.wpos >= ipcm.rpos) nsampl = ipcm.nsampl - ipcm.wpos;
+    else nsampl = ipcm.rpos - ipcm.wpos;
+
+#if 0
+    navail = (size_t)snd_pcm_avail_update(ipcm.pcm);
+#else
+    navail = nsampl;
+#endif
+    if (nsampl > navail) nsampl = navail;
+
+    off = ipcm.wpos * ipcm.scale;
+    err = snd_pcm_readi(ipcm.pcm, ipcm.buf + off, nsampl);
+    if (err < 0)
+    {
+      if (recover_xrun(ipcm.pcm, err)) PERROR_GOTO("", on_error_2);
+      err = 0;
+    }
+
+    ipcm.wpos += (size_t)err;
+    if (ipcm.wpos == ipcm.nsampl) ipcm.wpos = 0;
+
+    /* write opcm */
+
+    if (ipcm.wpos >= ipcm.rpos) nsampl = ipcm.wpos - ipcm.rpos;
+    else nsampl = ipcm.nsampl - ipcm.rpos;
+
+    off = ipcm.rpos * ipcm.scale;
+    err = snd_pcm_writei(opcm.pcm, ipcm.buf + ipcm.rpos, nsampl);
+    if (err < 0)
+    {
+      if (recover_xrun(opcm.pcm, err)) PERROR_GOTO("", on_error_2);
+      err = 0;
+    }
+
+    ipcm.rpos += (size_t)err;
+    if (ipcm.rpos == ipcm.nsampl) ipcm.rpos = 0;
   }
 
+  err = 0;
+
  on_error_2:
-  snd_pcm_close(out_pcm);
+  pcm_close(&opcm);
  on_error_1:
-  snd_pcm_close(in_pcm);
+  pcm_close(&ipcm);
  on_error_0:
-  return 0;
+  return err;
 }
